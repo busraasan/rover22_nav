1cddb808cc18fadb8969739763f91480153a2d9c
busraasan <busraasan2@gmail.com>
2021-11-19 22:42:10 +0300

    jj

---
+++ any_node/any_node/include/any_node/Param.hpp
@@ -0,0 +1,16 @@
+/*!
+ * @file	Param.hpp
+ * @author	Philipp Leemann
+ * @date	July, 2016
+ */
+
+#pragma once
+
+#include "param_io/get_param.hpp"
+#include "param_io/set_param.hpp"
+
+namespace any_node {
+
+using namespace param_io;  // NOLINT
+
+}  // namespace any_node
